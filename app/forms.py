65586d7537f2c1394dbfb1afaa897b556cddd995
Derek Robertson <derek.robertson.uk@gmail.com>
2021-07-22 08:48:11 +0100

    Created login form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,14 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from flask_wtf import FlaskForm, Form
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
+    email = StringField('Email address', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
+    remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
+
+class DatePickerForm(Form):
+    date = DateField(id='datepick')
